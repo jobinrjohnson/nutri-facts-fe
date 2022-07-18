60e7bc2903b72615323bab76a78b60cf8ca3a8ff
Sophiya Antony <sophiya.antony09@gmail.com>
2022-07-18 15:28:42 +0530

    added sidebar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import LoginPage from "./pages/login/LoginPage"
+import ProductListEmpty from './pages/ProductList_empty/ProductList';
 
 function App() {
   return (
-    <LoginPage />
+    <ProductListEmpty />
   );
 }
 
---
+++ src/pages/ProductList_empty/ProductList.jsx
@@ -0,0 +1,28 @@
+import './style.css';
+import Sidebar from '../../components/sidebar/Sidebar';
+
+function ProductListEmpty() {
+    return (
+        <div className='container-fluid vh-100'>
+
+
+            <div className='row'>
+                <div className='col-auto'>
+
+                    <Sidebar />
+
+                </div>
+                <div className='col'>
+                    <p>ashg\sgh</p>
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+    );
+}
+
+export default ProductListEmpty;
