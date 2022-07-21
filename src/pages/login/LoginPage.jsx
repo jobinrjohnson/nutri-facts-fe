4b37699e0c1afe2877696c43522dcba8b1da6aef
Jobin Johnson <jobinrjohnson@gmail.com>
2022-07-21 17:07:41 +0530

    login api integrated

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,28 +1,15 @@
 import './style.css';
 import loginPage from "../login/loginPage.png"
 import logo from "../../logo.png"
-import { Component } from 'react';
+import {Component} from 'react';
 
 class Login extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            email: "",
-            password: "",
-            token: "",
+            email: "", password: "", token: "",
         };
-    }
-
-    componentDidMount() {
-        this.fetchData();
-    }
-
-    fetchData() {
-        fetch("https://api.staging.restaurant.thefoodo.com/auth/login", requestOptions)
-            .then(response => response.text())
-            .then(result => console.log(result))
-            .catch(error => console.log('error', error));
     }
 
     change = (e) => {
@@ -32,55 +19,65 @@ class Login extends Component {
     };
 
 
-    onSubmit = (e) =>{
-        this.fetchData();
+    onSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
-        this.setState({
-             email: "",
-             password: "",
-            });
+        let myHeaders = new Headers();
 
+        myHeaders.append("Content-Type", "application/json");
+        let raw = JSON.stringify({
+            "email": this.state.email,
+            "password": this.state.password
+        });
+
+        let requestOptions = {
+            method: 'POST',
+            body: raw,
+            redirect: 'follow',
+            headers: myHeaders
         };
 
+        fetch("https://api.staging.restaurant.thefoodo.com/auth/login", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+
+    }
+
     render() {
-        return (
-            <div className='container-fluid vh-100 d-flex align-items-center justify-content-center' style={{
-                backgroundImage: `url(${loginPage})`,
-                backgroundSize: "100% 100%"
+        return (<div className='container-fluid vh-100 d-flex align-items-center justify-content-center' style={{
+            backgroundImage: `url(${loginPage})`, backgroundSize: "100% 100%"
+        }}>
+            <img
+                style={{
+                    position: "absolute", top: "20px", left: "20px", width: "200px"
+                }}
+                src={logo}/>
+
+            <div className='text-center' style={{
+                width: "340px"
             }}>
-                <img
-                    style={{
-                        position: "absolute",
-                        top: "20px",
-                        left: "20px",
-                        width: "200px"
-                    }}
-                    src={logo} />
 
-                <div className='text-center' style={{
-                    width: "340px"
-                }}>
+                <h1 className='sigin-spec'>Sign In</h1>
 
-                    <h1 className='sigin-spec'>Sign In</h1>
+                <p className="mb-3">Don’t have an account? <a href="#" className="text-reset small-signin-color">Sign
+                    up</a></p>
 
-                    <p className="mb-3">Don’t have an account? <a href="#" className="text-reset small-signin-color">Sign up</a></p>
-
-                    <div class="mb-3 text-start">
-                        <label className="form-label text-spec">Email </label>
-                        <input  name ='email' value={this.state.email} onChange={e => this.change(e) } type="email" className="form-control" placeholder="" />
-                    </div>
-                    <div class="mb-3 text-start">
-                        <label className="form-label text-spec">Password</label>
-                        <input name='password' value={this.state.password} onChange={e => this.change(e) } type="password" className="form-control" />
-                    </div>
-
-                    <p className='text-end text-muted2'>Forgot password?</p>
-                    <button onClick={e => this.onSubmit(e)} className='btn btn-primary w-100'>Sign In</button>
-
+                <div class="mb-3 text-start">
+                    <label className="form-label text-spec">Email </label>
+                    <input name='email' value={this.state.email} onChange={e => this.change(e)} type="email"
+                           className="form-control" placeholder=""/>
                 </div>
+                <div class="mb-3 text-start">
+                    <label className="form-label text-spec">Password</label>
+                    <input name='password' value={this.state.password} onChange={e => this.change(e)}
+                           type="password" className="form-control"/>
+                </div>
+
+                <p className='text-end text-muted2'>Forgot password?</p>
+                <button onClick={e => this.onSubmit(e)} className='btn btn-primary w-100'>Sign In</button>
+
             </div>
-        );
+        </div>);
     }
 
 }
